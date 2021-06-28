d91a9cdcfa2601d503f74208ff96f3590cdf0055
qiuxiaochi <1348270542@qq.com>
2021-06-28 17:24:11 +0800

    增加集合工具

--- app/src/main/java/com/example/note/home/HomeActivity.java
+++ app/src/main/java/com/example/note/home/HomeActivity.java
@@ -3,6 +3,7 @@ package com.example.note.home;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.KeyEvent;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -18,6 +19,15 @@ public class HomeActivity extends AppCompatActivity {
   }
 
   @Override
+  public boolean onKeyDown(int keyCode, KeyEvent event) {
+    if(keyCode == KeyEvent.KEYCODE_BACK){
+      moveTaskToBack(true);
+      return true;
+    }
+    return super.onKeyDown(keyCode, event);
+  }
+
+  @Override
   protected void onCreate(@Nullable Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     getSupportFragmentManager()
@@ -25,5 +35,4 @@ public class HomeActivity extends AppCompatActivity {
         .replace(android.R.id.content, new HomeTabFragment())
         .commit();
   }
-
 }
